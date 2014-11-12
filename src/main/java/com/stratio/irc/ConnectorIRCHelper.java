151dcaf28320f969c9eb7b4a5819e3a08b2e2e3f
jjlopezm <jjlopez@stratio.com>
2014-11-12 17:05:36 +0100

    fix little problems

--- src/main/java/com/stratio/irc/ConnectorIRCHelper.java
+++ src/main/java/com/stratio/irc/ConnectorIRCHelper.java
@@ -14,7 +14,7 @@ import java.util.Map;
 
 public class ConnectorIRCHelper implements IConnectorIRC {
 
-    private Map<String, List<Message>> messages = new HashMap<>();
+    private Map<String, List<Message>> messages = new HashMap<String,List<Message>>();
     String nick;
 
     @Override
